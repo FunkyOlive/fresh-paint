03fa3a536c044b9c5646c80e23fc6ff17764740d
Leo Biegert <6biegert@informatik.uni-hamburg.de>
2021-06-21 10:12:48 +0200

    add welcome message add TODOs

--- fresh_paint/Program.cs
+++ fresh_paint/Program.cs
@@ -20,8 +20,10 @@ namespace fresh_paint
             //TODO migrate txt => csv, then browse url attrs in csv
 
             string[] urlList = System.IO.File.ReadAllLines(urlListPath);
-            Console.WriteLine("Read input file " + urlListPath);
-            Console.WriteLine("There are " + urlList.LongLength + " saved image urls");
+
+            Console.WriteLine("fresh_paint by FunkyOlive\nPress ctrl+c at any time to exit.\n"); //TODO add version hint
+            Console.WriteLine("Read input file at: " + urlListPath);
+            Console.WriteLine("There are " + urlList.LongLength + " saved image urls.");
             Console.WriteLine("Enter index of image url to download...");
             int urlIndex;
             while (!int.TryParse(Console.ReadLine(), out urlIndex) | urlIndex >= urlList.LongLength)
@@ -30,16 +32,18 @@ namespace fresh_paint
                 Console.WriteLine("Enter index of image url to download...");
             }
             string url = urlList[urlIndex];
-            Console.WriteLine("Url at index " + urlIndex + " is " + url);
+            Console.WriteLine("Url at index " + urlIndex + " is: " + url);
             Console.WriteLine("Downloading..");
 
             string wallpaperPath = DownloadImageToTempBmp(url);
-            Console.WriteLine("Downloaded image to temp file path " + wallpaperPath);
+            Console.WriteLine("Downloaded image to temp file at: " + wallpaperPath);
 
             Console.WriteLine("Setting wallpaper..");
             Wallpaper.Set(wallpaperPath);
+            //TODO add Wallpaper.style options (tryout live while not touching wp history)
 
             //TODO add key to undo or to jump back in history with Wallpaper.RestoreState()
+            //TODO add key to run again
             Console.WriteLine("\n\nPress any key to exit...");
             Console.ReadKey();
         }
@@ -52,6 +56,7 @@ namespace fresh_paint
          */
         private static string DownloadImageToTempBmp(string url)
         {
+            //TODO add routine to clean out old temp files if recommended by windows guidelines or sth
             string TempFilePath = Path.GetTempFileName();
             Console.WriteLine(TempFilePath);
 
